3aa8c3130b022e154fa3436c434b4c625dac7d8c
Grace Lee <igracelee1@gmail.com>
2019-07-10 22:51:32 -0400

    componentDidUpdate for switching between # of posts and # of todos

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -12,9 +12,21 @@ class ResourceList extends React.Component {
     this.setState({ resources: response.data });
   }
 
+  async componentDidUpdate(prevProps) {
+    // console.log(prevProps); see if resource type changed in props during re-render; then make another request
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+
+      this.setState({ resources: response.data });
+    }
+  }
+
   render() {
     {/* return <div>{this.props.resource}</div>; */}
-    return <div>{this.state.resources.length}</div>; {/* will show how many elements we have in resources*/}
+    return <div>{this.state.resources.length}</div>;
+    {/* will show how many elements we have in resources*/}
   }
 }
 
