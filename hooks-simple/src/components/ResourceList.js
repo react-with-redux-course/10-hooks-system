ca635399385a252e8ebc159ad79f07e09d47df0b
Grace Lee <igracelee1@gmail.com>
2019-07-10 23:01:00 -0400

    useState and useEffect in place of componentDidMount and componentDidUpdate

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,33 +1,49 @@
-import React from "react";
+// import hooks from React library
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-class ResourceList extends React.Component {
-  state = { resources: [] };
+const ResourceList = () => {
+  const [resources, setResources] = useState([]);
 
-  async componentDidMount() {
+  const fetchResource = async () => {
     const response = await axios.get(
       `https://jsonplaceholder.typicode.com/${this.props.resource}`
     );
 
     this.setState({ resources: response.data });
-  }
+  };
 
-  async componentDidUpdate(prevProps) {
-    // console.log(prevProps); see if resource type changed in props during re-render; then make another request
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get(
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
+  return <div>{resources.length}</div>;
 
-      this.setState({ resources: response.data });
-    }
-  }
+};
 
-  render() {
-    {/* return <div>{this.props.resource}</div>; */}
-    return <div>{this.state.resources.length}</div>;
-    {/* will show how many elements we have in resources*/}
-  }
-}
+// class ResourceList extends React.Component {
+//   state = { resources: [] };
+
+//   async componentDidMount() {
+//     const response = await axios.get(
+//       `https://jsonplaceholder.typicode.com/${this.props.resource}`
+//     );
+
+//     this.setState({ resources: response.data });
+//   }
+
+//   async componentDidUpdate(prevProps) {
+//     // console.log(prevProps); see if resource type changed in props during re-render; then make another request
+//     if (prevProps.resource !== this.props.resource) {
+//       const response = await axios.get(
+//         `https://jsonplaceholder.typicode.com/${this.props.resource}`
+//       );
+
+//       this.setState({ resources: response.data });
+//     }
+//   }
+
+//   render() {
+//     {/* return <div>{this.props.resource}</div>; */}
+//     return <div>{this.state.resources.length}</div>;
+//     {/* will show how many elements we have in resources*/}
+//   }
+// }
 
 export default ResourceList;
